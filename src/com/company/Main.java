4a1838f6d73bb05030f21787f676ed6283e66613
dimansen68 <dimansen15@inbox.ru>
2020-12-11 17:29:33 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Написать программу для поиска минимального из двух чисел.
+        double a = 5;
+        double b = 6;
+        if (a>b){
+            System.out.println(b + " - это минимальное число из " + a + " и " + b); }
+            else {System.out.println(a + " - это минимальное число из " + a + " и " + b);
+                }
+    }
+}
